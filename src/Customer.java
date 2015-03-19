14f15e0151cf951616c8a7e5d4c0bec65c2d4da1
firethanatos <fire_thanatos@live.com>
2015-03-18 18:10:51 -0700

    +Customer +PurchaseItem +edits
    Added Customer and PurchaseItem java classes, changed constructors to
    setatribute instead of direct =

---
+++ src/Customer.java
@@ -0,0 +1,69 @@
+
+/**
+ * Class to represent the Customer(cid, password, customerName, address, phone) table
+ * @author Chazz Young
+ * @version 0.0
+ */
+public class Customer
+{
+    private String cid, customerName, password, address, phone;
+
+    public Customer(String cid, String customerName, String password, String address, String phone)
+    {
+        setCID(cid);
+        setCustomerName(customerName);
+        setPassword(password);
+        setAddress(address);
+        setPhone(phone);
+    }
+    
+    public String getCID()
+    {
+        return cid;
+    }
+    
+    public String getCustomerName()
+    {
+        return customerName;
+    }
+    
+    public String getPassword()
+    {
+        return password;
+    }
+    
+    public String getAddress()
+    {
+        return address;
+    }
+    
+    public String getPhone()
+    {
+        return phone;
+    }
+    
+    public void setCID(String newID)
+    {
+        cid = newID;
+    }
+    
+    public void setCustomerName(String newName)
+    {
+        customerName = newName;
+    }
+    
+    public void setPassword(String newPass)
+    {
+        password = newPass;
+    }
+    
+    public void setAddress(String newAddress)
+    {
+        address = newAddress;
+    }
+    
+    public void setPhone(String newPhone)
+    {
+        phone = newPhone;
+    }
+}
---
+++ src/PurchaseItem.java
@@ -0,0 +1,48 @@
+
+/**
+ * Class to represent the PurchaseItem(receiptId,  upc, quantity) table
+ * @author Chazz Young
+ * @version 0.0
+ */
+public class PurchaseItem extends Record
+{
+    private int receiptID, quantity;
+    private String upc;
+    
+    public PurchaseItem(int rid, int qty, String upc)
+    {
+        setReceiptID(rid);
+        setQuantity(qty);
+        setUPC(upc);
+    }
+    
+    public int getReceiptID()
+    {
+        return receiptID;
+    }
+    
+    public int getQuantity()
+    {
+        return quantity;
+    }
+    
+    public String getUPC()
+    {
+        return upc;
+    }
+    
+    public void setReceiptID(int rid)
+    {
+        receiptID = rid;
+    }
+    
+    public void setQuantity(int qty)
+    {
+        quantity = qty;
+    }
+    
+    public void setUPC(String upc)
+    {
+        this.upc = upc;
+    }
+}
